e05693c1ec3866022e33e610a1045542c728c7c3
eliewebavenue <elie@webavenue.agency>
2024-12-03 14:57:24 +0100

    Include header and footer

--- project/views/homeView.php
+++ project/views/homeView.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <div class="container">
-        Hello, C'est la page d'accueil !!
-    </div>
-</body>
-</html>
+<?php
+include __DIR__ . '/partials/header.php';
+?>
+<div class="container">
+    Hello, C'est la page d'accueil !!
+</div>
+<?php
+include __DIR__ . '/partials/footer.php';
+?>
--- project/views/registerView.php
+++ project/views/registerView.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
+<?php
+$title = "Inscription";
+include __DIR__ . '/partials/header.php';
+?>
 <section class="vh-100" style="background-color: #eee;">
   <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
@@ -75,5 +70,6 @@
     </div>
   </div>
 </section>
-</body>
-</html>
+<?php
+include __DIR__ . '/partials/footer.php';
+?>
