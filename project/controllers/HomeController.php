af0f8bfe1be228d05719014cdc52f39744106df1
eliewebavenue <elie@webavenue.agency>
2024-12-03 15:22:05 +0100

    display navbar and links if is connected or not

--- project/controllers/HomeController.php
+++ project/controllers/HomeController.php
@@ -3,6 +3,11 @@ require_once __DIR__ . '/../core/Controller.php';
 
 class HomeController extends Controller {
     function index() {
-        $this->render('home');
+        if(isset($_SESSION['user'])){
+            $this->render('home');
+        }else{
+            header("Location: index.php?action=login&entity=auth");
+            exit;
+        }
     }
 }
--- project/views/partials/header.php
+++ project/views/partials/header.php
@@ -7,3 +7,71 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+        <!-- Toggle button -->
+        <button
+        data-mdb-collapse-init
+        class="navbar-toggler"
+        type="button"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+        <i class="fas fa-bars"></i>
+        </button>
+
+        <!-- Collapsible wrapper -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+            <img
+            src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
+            height="15"
+            alt="MDB Logo"
+            loading="lazy"
+            />
+        </a>
+        <!-- Left links -->
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <?php if (isset($_SESSION['user'])): ?>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Accueil</a>
+                </li>
+            <?php endif; ?>
+        </ul>
+        <!-- Left links -->
+        </div>
+        <!-- Collapsible wrapper -->
+
+        <!-- Right elements -->
+        <div class="d-flex align-items-center">
+            <?php if (isset($_SESSION['user'])): ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#"><?php echo $_SESSION['user']['username'] ?></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Se déconnecter</a>
+                    </li>
+                </ul>
+            <?php else :  ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                    <a class="nav-link" href="?action=login&entity=auth">Connexion</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="?action=register&entity=auth">Inscription</a>
+                    </li>
+                </ul>
+            <?php endif;  ?>
+            
+        </div>
+        <!-- Right elements -->
+    </div>
+    <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
