55a4d2451008e35eb1a936e00e92338734fba4f5
eliewebavenue <elie@webavenue.agency>
2024-12-03 14:41:58 +0100

    fix warning error

--- project/models/UserModel.php
+++ project/models/UserModel.php
@@ -5,10 +5,9 @@ require_once __DIR__ . '/../core/Model.php';
 class UserModel extends Model{
 
     function register($username, $email, $password) {
-        $bdd = $this->pdo;
-        $req = $bdd->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
-
+        $req = $this->pdo->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
         $res = $req->execute([$username, $email, $password]);
+
         if($res) {
             return true;
         } else {
@@ -17,9 +16,8 @@ class UserModel extends Model{
     }
 
     function login($username, $password){
-        $bdd = $this->pdo;
-        $req = $bdd->prepare("SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?");
-        $res = $req->execute([$username, $username, $password]);
+        $this->pdo->prepare("SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?");
+        $req->execute([$username, $username, $password]);
 
         $user = $req->fetch(PDO::FETCH_ASSOC);
 
