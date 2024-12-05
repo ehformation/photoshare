1304cf7c821123486daad2df620c05ab419eb934
eliewebavenue <elie@webavenue.agency>
2024-12-05 11:31:54 +0100

    add custom style for photoshare

--- project/views/partials/header.php
+++ project/views/partials/header.php
@@ -6,6 +6,7 @@
     <title><?php echo $title ?? 'Accueil'; ?> - PhotoShare</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <!-- Navbar -->
